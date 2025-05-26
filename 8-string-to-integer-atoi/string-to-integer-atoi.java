db71998a4b6d61e56e1700c678ad3f390f32b31c
Mohare786-Kunal <128957133+Mohare786-Kunal@users.noreply.github.com>
2025-05-26 22:33:00 +0530

    Time: 1 ms (100.00%) | Memory: 42.9 MB (16.26%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int myAtoi(String s) {
+        s = s.trim();
+        if (s.isEmpty()) return 0;
+        int sign = 1, i = 0, result = 0;
+        if (s.charAt(0) == '-') { sign = -1; i++; }
+        else if (s.charAt(0) == '+') i++;
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            result = result * 10 + digit;
+            i++;
+        }
+        return result * sign;
+    }
+}
