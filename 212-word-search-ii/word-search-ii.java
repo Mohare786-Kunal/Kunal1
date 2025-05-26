a40d2a2b36eacc65eece16257995b2553ec27282
Mohare786-Kunal <128957133+Mohare786-Kunal@users.noreply.github.com>
2025-05-26 23:42:11 +0530

    Time: 159 ms (73.67%) | Memory: 45.3 MB (52.71%) - LeetSync

---
+++ 212-word-search-ii/word-search-ii.java
@@ -0,0 +1,49 @@
+class Solution {
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        String word;
+    }
+    
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> result = new ArrayList<>();
+        TrieNode root = buildTrie(words);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs(board, i, j, root, result);
+            }
+        }
+        return result;
+    }
+    
+    private TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for (String word : words) {
+            TrieNode node = root;
+            for (char c : word.toCharArray()) {
+                int index = c - 'a';
+                if (node.children[index] == null) node.children[index] = new TrieNode();
+                node = node.children[index];
+            }
+            node.word = word;
+        }
+        return root;
+    }
+    
+    private void dfs(char[][] board, int i, int j, TrieNode node, List<String> result) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) return;
+        char c = board[i][j];
+        if (c == '#' || node.children[c - 'a'] == null) return;
+        node = node.children[c - 'a'];
+        if (node.word != null) {
+            result.add(node.word);
+            node.word = null;
+        }
+        char temp = board[i][j];
+        board[i][j] = '#';
+        dfs(board, i + 1, j, node, result);
+        dfs(board, i - 1, j, node, result);
+        dfs(board, i, j + 1, node, result);
+        dfs(board, i, j - 1, node, result);
+        board[i][j] = temp;
+    }
+}
