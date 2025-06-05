6a700d411087b303e7428363ee0ee94c830e6106
Mohare786-Kunal <128957133+Mohare786-Kunal@users.noreply.github.com>
2025-06-05 23:37:46 +0530

    Time: 1 ms (51.87%) | Memory: 42.7 MB (65.08%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,16 +1,28 @@
+import java.util.List;
+import java.util.ArrayList;
+
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
+
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), nums, 0);
+
+        List<Integer> current = new ArrayList<>();
+
+        subsequence(0, nums, current, result);
+        
         return result;
     }
     
-    private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] nums, int start) {
-        result.add(new ArrayList<>(temp));
-        for (int i = start; i < nums.length; i++) {
-            temp.add(nums[i]);
-            backtrack(result, temp, nums, i + 1);
-            temp.remove(temp.size() - 1);
+    public void subsequence(int index, int[] nums, List<Integer> current, List<List<Integer>> result) {
+        if (index >= nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
         }
+        
+        current.add(nums[index]);
+        subsequence(index + 1, nums, current, result);
+        
+        current.remove(current.size() - 1);
+        subsequence(index + 1, nums, current, result);
     }
 }
