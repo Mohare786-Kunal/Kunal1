d57ed58d88d60edfbc904ac15632de0c2898eb16
Mohare786-Kunal <128957133+Mohare786-Kunal@users.noreply.github.com>
2025-06-05 23:40:09 +0530

    Time: 1 ms (51.87%) | Memory: 42.7 MB (82.13%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -2,18 +2,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-
-        List<List<Integer>> result = new ArrayList<>();
-
-        List<Integer> current = new ArrayList<>();
-
-        subsequence(0, nums, current, result);
-        
-        return result;
-    }
-    
-    public void subsequence(int index, int[] nums, List<Integer> current, List<List<Integer>> result) {
+     public void subsequence(int index, int[] nums, List<Integer> current, List<List<Integer>> result) {
         if (index >= nums.length) {
             result.add(new ArrayList<>(current));
             return;
@@ -25,4 +14,18 @@ class Solution {
         current.remove(current.size() - 1);
         subsequence(index + 1, nums, current, result);
     }
+
+
+    public List<List<Integer>> subsets(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        List<Integer> current = new ArrayList<>();
+
+        subsequence(0, nums, current, result);
+        
+        return result;
+    }
+    
+   
 }
