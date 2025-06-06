6469627ced1985401e540460e0a126f889c0847c
Mohare786-Kunal <128957133+Mohare786-Kunal@users.noreply.github.com>
2025-06-06 19:12:01 +0530

    Time: 3 ms (74.78%) | Memory: 43.1 MB (94.36%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,22 +1,26 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         Arrays.sort(candidates);
-        backtrack(result, new ArrayList<>(), candidates, target, 0);
+        
+        combinationSum2(0, candidates, list, result, target);
         return result;
     }
     
-    private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] candidates, int target, int start) {
+    public void combinationSum2(int ind, int[] nums, List<Integer> list, List<List<Integer>> result, int target) {
         if (target == 0) {
-            result.add(new ArrayList<>(temp));
+            result.add(new ArrayList<>(list));
             return;
         }
-        for (int i = start; i < candidates.length; i++) {
-            if (i > start && candidates[i] == candidates[i - 1]) continue;
-            if (candidates[i] > target) break;
-            temp.add(candidates[i]);
-            backtrack(result, temp, candidates, target - candidates[i], i + 1);
-            temp.remove(temp.size() - 1);
+        
+        for (int i = ind; i < nums.length; i++) {
+            if (i > ind && nums[i] == nums[i-1]) continue;
+            if (nums[i] > target) break;
+            
+            list.add(nums[i]);
+            combinationSum2(i + 1, nums, list, result, target - nums[i]);
+            list.remove(list.size() - 1);
         }
     }
 }
