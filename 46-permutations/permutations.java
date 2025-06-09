5e6b72e70de4aed891bd2a03b330f0fe1e36906e
Mohare786-Kunal <128957133+Mohare786-Kunal@users.noreply.github.com>
2025-06-09 22:17:02 +0530

    Time: 1 ms (93.49%) | Memory: 44.5 MB (64.00%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -3,25 +3,28 @@ class Solution {
         Arrays.sort(nums); 
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        boolean[] freq = new boolean[nums.length];
+
         
-        permutationsI(0, nums, list, result, freq);
+        permutationsI(0, nums, list, result);
         return result;
     }
-    public static void permutationsI(int ind, int[] nums, List<Integer> list, List<List<Integer>> result, boolean[] freq) {
-        if(list.size() == nums.length){
+    public static void permutationsI(int ind, int[] nums, List<Integer> list, List<List<Integer>> result ) {
+        if(ind == nums.length){
             result.add(new ArrayList<>(list));
             return;
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (!freq[i]) {
-                list.add(nums[i]);
-                freq[i] = true;
-                permutationsI(ind + 1, nums, list, result, freq);
-                list.remove(list.size() - 1);
-                freq[i] = false;
-            }
+        for (int i = ind; i < nums.length; i++) {
+            swap(nums, ind, i);
+            list.add(nums[ind]);  // Add the number at current position
+            permutationsI(ind + 1, nums, list, result);
+            list.remove(list.size() - 1);
+            swap(nums, ind, i);  // Undo the swap
         }
+    }
+    public static void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
