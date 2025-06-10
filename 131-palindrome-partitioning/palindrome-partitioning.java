e845cd2e76f6b93e46fc4fe76af716fe5023198f
Mohare786-Kunal <128957133+Mohare786-Kunal@users.noreply.github.com>
2025-06-10 18:01:33 +0530

    Time: 9 ms (27.47%) | Memory: 57.3 MB (12.61%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,31 +1,26 @@
 class Solution {
 
-       public static boolean isPalindrome(int start, int end, String s) {
-        while (start <= end) {
-            if (s.charAt(start) != s.charAt(end)) {
-                return false;
-            }
-            start++;
-            end--;
+        public static boolean isPalindrome(String s, int start, int end) {
+        if (start >= end) {
+            return true;
         }
-        return true;
+        return s.charAt(start) == s.charAt(end) && isPalindrome(s, start + 1, end - 1);
     }
     
     public static void partitioning(int ind, String s, List<String> path, List<List<String>> result) {
-        if(ind ==s.length()){
+        if (ind == s.length()) {
             result.add(new ArrayList<>(path));
             return;
         }
         
         for (int i = ind; i < s.length(); i++) {
-            if(isPalindrome(ind, i, s)){
-                path.add(s.substring(ind, i+1));
+            if (isPalindrome(s, ind, i)) {
+                path.add(s.substring(ind, i + 1));
                 partitioning(i + 1, s, path, result);
                 path.remove(path.size() - 1);
             }
         }
     }
-
     public List<List<String>> partition(String s) {
         List<List<String>> result =  new ArrayList<>();
         List<String> path = new ArrayList<>();
